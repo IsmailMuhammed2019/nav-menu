99718e7ce9125ad67fc96a37886e987cf63cbbf4
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-02-16 12:54:10 +0100

    display logo, menu and social icons

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,17 +4,38 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-
  return (
   <>
   <nav>
    <div className="nav-center">
     <div className="nav-header">
      <img src={logo} alt="logo" />
-     <button className="nav-toggele">
+     <button className="nav-toggle" >
       <FaBars />
      </button>
     </div>
+    <div className="links-container show-container">
+     <ul className="links">
+      {links.map((link) => {
+       const {id, url, text} = link
+       return (
+        <li key={id}>
+         <a href={url}>{text}</a>
+        </li>
+       )
+      })}
+     </ul>
+    </div>
+    <ul className="social-icons">
+     {social.map((icons) => {
+      const {id, url, icon} = icons
+      return (
+       <li key={id}>
+        <a href={url}>{icon}</a>
+       </li>
+      )
+     })}
+    </ul>
    </div>
   </nav>
   </>
